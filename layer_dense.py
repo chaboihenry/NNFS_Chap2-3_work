33c21afe4bff55e5686ae5fc794a4de357792ebf
chaboihenry <72213708+chaboihenry@users.noreply.github.com>
2025-07-06 15:57:05 -0600

    Delete layer_dense.py

--- layer_dense.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-class Layer_Dense:
-    def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1, n_neurons))
-    def forward(self, inputs):
-        self.output = np.dot(np.array(inputs), self.weights) + self.biases 
