9a9c9e975e22d15ab5611e88f883515d0e4018e1
chaboihenry <72213708+chaboihenry@users.noreply.github.com>
2025-07-06 16:36:16 -0600

    Update test_layers.py

--- testing/test_layers.py
+++ testing/test_layers.py
@@ -1,9 +1,5 @@
 """Tests for the Layer_Dense class."""
 
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 import numpy as np
 import pytest
 from src.layer import Layer_Dense
