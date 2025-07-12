7475772c8fc62c4367033a83e20a654dcc6f7bf5
chaboihenry <72213708+chaboihenry@users.noreply.github.com>
2025-07-12 16:03:54 -0400

    Update main_spiral.py

--- main_scripts/main_spiral.py
+++ main_scripts/main_spiral.py
@@ -95,12 +95,60 @@ def main():
     
     # Print accuracy
     print(accuracy)
+
+    #########################################################
+    # Chapter 6 final code output
+    #########################################################
+   
+    X, y = spiral_data(samples=100, classes=3)
+    dense1 = Layer_Dense(2, 3)
+    activation1 = Activation_ReLU()
+    dense2 = Layer_Dense(3, 3)
+    activation2 = Activation_Softmax()
+    loss_function = Loss_CategoricalCrossentropy()
     
-    # Plot the data
-    plt.scatter(X[:, 0], X[:, 1], c=y, cmap='brg')
-    plt.title('Spiral Data')
-    plt.show()
+    lowest_loss = 99999
+    # Best weights and biases found so far
+    best_dense1_weights = dense1.weights.copy()
+    best_dense1_biases = dense1.biases.copy()
+    best_dense2_weights = dense2.weights.copy()
+    best_dense2_biases = dense2.biases.copy()
+    
+    for iteration in range(10000):
+        
+        # Update weights and biases
+        dense1.weights += 0.05 * np.random.randn(2, 3)
+        dense1.biases += 0.05 * np.random.randn(1, 3)
+        dense2.weights += 0.05 * np.random.randn(3, 3)
+        dense2.biases += 0.05 * np.random.randn(1, 3)
+        
+        # Perform forward pass
+        dense1.forward(X)
+        activation1.forward(dense1.output)
+        dense2.forward(activation1.output)
+        activation2.forward(dense2.output)
+        
+        # Calculate loss
+        loss = loss_function.calculate(activation2.output, y)
+        
+        # Calculate accuracy from output of act2 and targets
+        predictions = np.argmax(activation2.output, axis=1)
+        accuracy = np.mean(predictions == y)
+        
+        # If loss is smaller - print and save weights and biases aside
+        if loss < lowest_loss:
+            print(f'New set of weights found, iteration: {iteration} loss: {loss:.7f} acc: {accuracy}')
+            best_dense1_weights = dense1.weights.copy()
+            best_dense1_biases = dense1.biases.copy()
+            best_dense2_weights = dense2.weights.copy()
+            best_dense2_biases = dense2.biases.copy()
+            lowest_loss = loss
+        # Revert weights and biases
+        else:
+            dense1.weights = best_dense1_weights.copy()
+            dense1.biases = best_dense1_biases.copy()
+            dense2.weights = best_dense2_weights.copy()
+            dense2.biases = best_dense2_biases.copy()
 
 if __name__ == "__main__":
     main() 
-
